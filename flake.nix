83523ecdbc998c591085c5433a8ecda038ef860c
David Huu Pham <dayvidpham@gmail.com>
2024-12-04 15:55:38 -0800

    feat(nix): adds Rust toolchains to Nix dev shell

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
     nixpkgs.url = "github:NixOS/Nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/Nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
   };
 
   outputs =
@@ -10,17 +14,26 @@
     , nixpkgs
     , nixpkgs-unstable
     , flake-utils
+    , fenix
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages."${system}";
       lib = pkgs.lib;
 
+      fenixPkgs = fenix.packages."${system}";
+      rust-channel = fenixPkgs.default;
+      rust-toolchain = rust-channel.toolchain;
+      rust-stdlib = rust-channel.rust-std;
+
       shell = pkgs.mkShell {
         packages = [
-          pkgs.cargo
-          pkgs.rustc
+          rust-toolchain
+          fenixPkgs.rust-analyzer
         ];
+        RUST_SRC_PATH = "${rust-stdlib}/lib/rustlib/x86_64-unknown-linux-gnu/lib";
+
+        allowSubstitutes = false;
       };
     in
     {
