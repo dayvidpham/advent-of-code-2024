bbd253229e8aed951258a5ef7cb505dd863e20f8
David Huu Pham <dayvidpham@gmail.com>
2024-12-04 20:56:49 -0800

    fix(nix): sets path to rust-src stdlib correctly using rust-src

--- flake.nix
+++ flake.nix
@@ -24,14 +24,14 @@
       fenixPkgs = fenix.packages."${system}";
       rust-channel = fenixPkgs.default;
       rust-toolchain = rust-channel.toolchain;
-      rust-stdlib = rust-channel.rust-std;
+      rust-stdlib = "${fenixPkgs.complete.rust-src}/lib/rustlib/src/rust/library";
 
       shell = pkgs.mkShell {
         packages = [
           rust-toolchain
           fenixPkgs.rust-analyzer
         ];
-        RUST_SRC_PATH = "${rust-stdlib}/lib/rustlib/x86_64-unknown-linux-gnu/lib";
+        RUST_SRC_PATH = rust-stdlib;
 
         allowSubstitutes = false;
       };
